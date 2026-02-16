7c2dde40e29f00b5fee29d5b1efbf4c18e37ef65
SonLy1016 <sonly@udel.edu>
2026-02-15 21:21:57 -0500

    please work

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,59 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>
+                    UD CISC275 with React Hooks and TypeScript Amazing Website
+                    by Sonny Ly
+                </h1>
+                <img
+                    src="C:\Users\User\Downloads\shocked face.jpg"
+                    alt="Shocked face"
+                />
             </header>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            <ul>
+                <li>Chicken</li>
+                <li>Bun</li>
+                <li>Ketchup</li>
+            </ul>
+
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
