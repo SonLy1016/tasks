788d21706c234a5594f6b43dd8755fdfcfe74360
SonLy1016 <sonly@udel.edu>
2026-03-15 16:27:50 -0400

    changes

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,23 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    const correct = answer === expectedAnswer;
+
     return (
         <div>
             <h3>Check Answer</h3>
+
+            <input
+                type="text"
+                value={answer}
+                onChange={(e) => {
+                    setAnswer(e.target.value);
+                }}
+            />
+
+            <div>{correct ? "✔️" : "❌"}</div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    const correct = choice === expectedAnswer;
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <select
+                value={choice}
+                onChange={(e) => {
+                    setChoice(e.target.value);
+                }}
+            >
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+
+            <div>{correct ? "✔️" : "❌"}</div>
         </div>
     );
 }
